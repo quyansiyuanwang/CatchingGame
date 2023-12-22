29fff811699da0436e5ef57dd7713a3f59201b3b
quyansiyuanwang <132176401+quyansiyuanwang@users.noreply.github.com>
2023-12-22 10:23:13 +0800

    perf: Normative code
    normative all code files

--- SupportBases.py
+++ SupportBases.py
@@ -1,13 +1,15 @@
-import threading
+import threading as threadingLib
 import time
-from typing import List, Dict, Tuple, Final, TypeVar
+from typing import Dict, Tuple, Final
 
+from ItemsOperator import Floor
 
-Location = Toward = TypeVar(Tuple[int, int])
-Digit = TypeVar((int, float))
+Location = Toward = Tuple[int, int]
+Digit = int | float
+
 
 class GameStateConsts:
-    MeetGhost: Final[str] = 'You was catched by ghost'
+    MeetGhost: Final[str] = 'You was caught by ghost'
     ReachExit: Final[str] = 'You Win!'
     GameRunning: Final[str] = 'Meet wall, the game continue'
     crashWall: Final[str] = 'Crash to the Wall'
@@ -27,7 +29,7 @@ class Map(list):
         super().__init__(arg)
         self.a = a
         self.b = b
-        
+
     def __contains__(self, location: Location) -> bool:
         """判断坐标是否位于对象内
 
@@ -41,15 +43,15 @@ class Map(list):
         if (x < 0 or x >= self.b) or (y < 0 or y >= self.a):
             return False
         return True
-    
+
     def __getitem__(self, __location: Location) -> any:
         x, y = __location
         return super().__getitem__(x)[y]
-    
+
     def __setitem__(self, __location: Location, __value: any) -> None:
         x, y = __location
         super().__getitem__(x)[y] = __value
-    
+
     def __str__(self):
         board = '-' * 15 + '\n'
         for line in self:
@@ -57,7 +59,7 @@ class Map(list):
                 board += item.__repr__() + ' '
             board += '\n'
         return board
-    
+
     # 相关功能methods
     def update(self, dictionary: Dict[Location, any]) -> bool:
         """使用字典更新地图内容
@@ -73,12 +75,12 @@ class Map(list):
             bool: 是否更新成功 T/F
         """
         for location, player in dictionary.items():
-            if type(self[location]) == 'Floor':
+            if isinstance(self[location], Floor):
                 return False
-            
+
             self[location] = player
         return True
-        
+
     def isFullOf(self, target: any) -> bool:
         """判断地图是否存在非target因素
         warning: 该判断逻辑为逻辑运算符 `=`,非 `is`
@@ -90,8 +92,8 @@ class Map(list):
             bool: 是否填满`target` T/F
         """
         return not any(any(item != target for item in line) for line in self)
-        
-    def isExist(self, target:any) -> bool:
+
+    def isExist(self, target: any) -> bool:
         """判断地图是否存在target因素
         warning: 该判断逻辑为运算 `in`
         
@@ -102,88 +104,91 @@ class Map(list):
             bool: 是否存在`target` T/F
         """
         return any(target in line for line in self)
-    
 
-class ClockBase(threading.Thread):
-    def __init__(self, threadings: list, strick_time: int, stop: bool = False):
+
+class ClockBase(threadingLib.Thread):
+    def __init__(self, threading: list, struck_time: int, stop: bool = False):
         """Clock事件耦合触发器
 
         Args:
-            threadings (list): 线程事件
-                example: `[[strick_time, increment_time, threading, pop, kwargs], ...]`
+            threading (list): 线程事件
+                example: `[[struck_time, increment_time, threading, pop, kwargs], ...]`
                     args:
-                        `strick_time`: 触发时间, 当`now`大于该值时触发事件
-                        `increment_time`: 时间增量, 当`pop`为`False`时, 更新`strick_time`(`+=`)
+                        `struck_time`: 触发时间, 当`now`大于该值时触发事件
+                        `increment_time`: 时间增量, 当`pop`为`False`时, 更新`struck_time`(`+=`)
                         `threading`: 触发的事件, 请注意并不提供相关参数传递方法, 
-                            若有需求请使用闭包函数`closurer`
+                            若有需求请使用闭包函数`closure`
                         `pop`: 触发事件次数, `-1`为无限
-            strick_time (int): 下一次触发的时间
+            struck_time (int): 下一次触发的时间
         """
         super().__init__()
-        self.threadings = threadings  # 更新线程CallBack
-        self.strick_time = strick_time  # 检测更新时间
+        self.threading = threading  # 更新线程CallBack
+        self.struck_time = struck_time  # 检测更新时间
         self.__stop = stop
-        
+
         self.relative_start = time.perf_counter() * 1000
-    
+
     def thread_add(self, thread):
-        """thread example:`[strick_time, increment_time, threading, pop, kwargs]`"""
-        self.threadings.append(thread)
-    
+        """thread example:`[struck_time, increment_time, threading, pop, kwargs]`"""
+        self.threading.append(thread)
+
     @property
     def now(self):
         return time.perf_counter() * 1000 - self.relative_start  # 相对时间(ms)
-    
+
     def stop(self):
         self.__stop = True
-    
+
     def trigger(self):
         pop_stack = []
-        for idx, (strick_time, increment_time, threading, pop, kwargs) in enumerate(self.threadings):
+        for idx, (struck_time, increment_time, threading, pop, kwargs) in enumerate(self.threading):
             # TODO 
-            if self.now > strick_time:
-                self.threadings[idx][0] += increment_time
+            if self.now > struck_time:
+                self.threading[idx][0] += increment_time
                 yield threading()
-            
-                self.threadings[idx][3] -= 1
-                if self.threadings[idx][3] == 0:
+
+                self.threading[idx][3] -= 1
+                if self.threading[idx][3] == 0:
                     pop_stack.append(idx)
-            
-            if self.now < strick_time: break
-                
+
+            if self.now < struck_time:
+                break
+
         while pop_stack:
             tar_idx = pop_stack.pop()
-            self.threadings.pop(tar_idx)
-            
-        self.threadings.sort(key=lambda x: x[0])
+            self.threading.pop(tar_idx)
 
-        yield (not not self.threadings, 'Everything is alright')  # -可暂时设为False用于测试
-        
+        self.threading.sort(key=lambda x: x[0])
+
+        yield not not self.threading, 'Everything is alright'  # -可暂时设为False用于测试
+
     def run(self):
         # TODO
         while True:
-            if self.__stop: exit(self.__stop)
+            if self.__stop:
+                exit(self.__stop)
             results = self.trigger()
-            
+
             for result in results:
                 if result is not None:
-                    res, infos = result
+                    res, info = result
                     if not res:
                         return
-                    
-            time.sleep(self.strick_time // 1000)
-            
+
+            time.sleep(self.struck_time // 1000)
+
     def __str__(self):
-        return f'{self.threadings}'
-    
-        
-def closurer(func: callable, *args, **kwargs):
+        return f'{self.threading}'
+
+
+def closure_device(func: callable, *args, **kwargs):
     def inclusion():
         return func(*args, **kwargs)
-    
+
     return inclusion
 
-def speed_time_disposer(value):
+
+def speed_time_disposer(value: Digit) -> Digit:
     """速度转等待时间(ms), 等待时间转速度
 
     Args:
@@ -194,9 +199,10 @@ def speed_time_disposer(value):
     """
     return 1000 / value
 
+
 def _test():
     c = ClockBase([], speed_time_disposer(30))
-    c.thread_add([0, speed_time_disposer(1), closurer(print, 'hello'), 5, {}])
+    c.thread_add([0, speed_time_disposer(1), closure_device(print, 'hello'), 5, {}])
     c.start()
     t = 5
     while t > 0:
@@ -204,8 +210,7 @@ def _test():
         time.sleep(1)
         t -= 1
     c.join()
-    
+
 
 if __name__ == '__main__':
     _test()
-    
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from random import choice
 import ItemsOperator
 import SupportBases
 from SupportBases import speed_time_disposer as sTD
-from SupportBases import closurer
+from SupportBases import closure_device
 
 
 def create_map(a: int, b: int):
     return SupportBases.Map(
-        ([ItemsOperator.Floor() for _ in range(a)] for _ in range(b)), 
+        ([ItemsOperator.Floor() for _ in range(a)] for _ in range(b)),
         a, b
-        )
+    )
+
 
 def keyboard_reflect(key: str):
     refl = {
@@ -19,8 +20,9 @@ def keyboard_reflect(key: str):
         's': (1, 0),
         'd': (0, 1)
     }
-    
+
     return refl.get(key, None)
+
 
 def user_enter_monitor():
     while True:
@@ -31,32 +33,34 @@ def user_enter_monitor():
         except KeyboardInterrupt:
             pass
 
+
 def test():
     m = create_map(6, 6)
-    c = SupportBases.ClockBase([], sTD(30)) # FPS
-    p = ItemsOperator.Player(map=m, spawn=(0, 0), blood=100, speed=100, clock=c)
-    g = ItemsOperator.Ghost(map=m, spawn=(4, 4), blood=100, speed=80, clock=c)
-    
+    c = SupportBases.ClockBase([], sTD(30))  # FPS
+    p = ItemsOperator.Player(game_map=m, spawn=(0, 0), blood=100, speed=100, clock=c)
+    g = ItemsOperator.Ghost(game_map=m, spawn=(4, 4), blood=100, speed=80, clock=c)
+
     def ghost_move():
-        x, y = g.location
-        res, info = g.move(keyboard_reflect(choice(('a', 'w', 's', 'd'))))
+        res, g_info = g.move(keyboard_reflect(choice(('a', 'w', 's', 'd'))))
         if not res:
-            print(info)
+            print(g_info)
             exit()
-        
+
     c.thread_add([500, 80, ghost_move, -1, {}])
-    c.thread_add([0, sTD(30), closurer(print, m), -1, {}])
+    c.thread_add([0, sTD(30), closure_device(print, m), -1, {}])
     c.start()
-    
+
     while True:
         toward = keyboard_reflect(user_enter_monitor())
         if toward is not None:
             running, info = p.move(toward)
             if not running:
                 break
-        
+
     c.stop()
+    c.join()
     print(info)
-    
+
+
 if __name__ == '__main__':
     test()
